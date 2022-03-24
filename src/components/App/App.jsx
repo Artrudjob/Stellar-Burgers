baa69288356ec4d2c05c24f115b12e2a74a38c90
Artrudjob <Artrudjob@yandex.ru>
2022-03-24 16:46:22 +0300

    feat: создать компонент BurgerConstructor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import appStyle from './app.module.css';
 import AppHeader from "../AppHeader/AppHeader";
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 
 function App() {
   return (
