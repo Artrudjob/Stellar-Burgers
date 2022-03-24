1cda502cc376a5e5389974f8d43ed768da50afdd
Artrudjob <Artrudjob@yandex.ru>
2022-03-24 16:57:39 +0300

    feat: создать data.js с массивом данных

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from "../../utils/data";
 import appStyle from './app.module.css';
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
