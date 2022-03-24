4e9a287230ca57ae1c8f440650e9dd2f2391c714
Artrudjob <Artrudjob@yandex.ru>
2022-03-24 16:52:08 +0300

    refactor: добавить атрибут type и стиль cursor

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -8,25 +8,25 @@ function AppHeader() {
     return (
         <section className={appHeaderStyle.header}>
             <header className={appHeaderStyle.header__container}>
-                <div className={appHeaderStyle.header__containerButton }>
-                    <button className={`${appHeaderStyle.header__button} pl-4 pr-4 pb-4 pt-4`}>
+                <nav className={appHeaderStyle.header__containerButton }>
+                    <button className={`${appHeaderStyle.header__button} pl-4 pr-4 pb-4 pt-4`} type={"button"}>
                         <BurgerIcon type={"primary"} />
                         <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Конструктор</p>
                     </button>
-                    <button className={`${appHeaderStyle.header__button} pl-4`}>
+                    <button className={`${appHeaderStyle.header__button} pl-4`} type={"button"}>
                         <ListIcon type="primary" />
                         <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Лента заказов</p>
                     </button>
-                </div>
+                </nav>
                 <div>
                     <Logo />
                 </div>
-                <div className={appHeaderStyle.header__containerButton}>
-                    <button className={`${appHeaderStyle.header__button}`}>
+                <nav className={appHeaderStyle.header__containerButton}>
+                    <button className={`${appHeaderStyle.header__button}`} type={"button"}>
                         <ProfileIcon type={"primary"} />
                         <p className={`text text_type_main-default  p-2 ${appHeaderStyle.header__text}`}>Личный кабинет</p>
                     </button>
-                </div>
+                </nav>
             </header>
         </section>
     )
