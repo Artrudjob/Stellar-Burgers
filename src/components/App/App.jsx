ce9b726e115186f897dd068a8addfb0937034734
Artrudjob <Artrudjob@yandex.ru>
2022-03-30 19:02:18 +0300

    refactor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import data from "../../utils/data";
 import appStyle from './app.module.css';
 import AppHeader from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
-import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
+import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 
 function App() {
   return (
