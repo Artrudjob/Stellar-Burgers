eace3cc43a9ac0ce426eccf5b36a785609825cb9
Artrudjob <Artrudjob@yandex.ru>
2022-04-11 16:06:08 +0300

    refactor: change component BurgerConstructor

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -4,7 +4,58 @@ import constructorStyle from './burgerConstructor.module.css'
 import { ConstructorElement, DragIcon, CurrencyIcon, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function BurgerConstructor(props) {
-    const constructorElements = props.arrData.map(element => {
+    const topBun = props.arrData.map(element => {
+        if (element.name === 'Краторная булка N-200i') {
+            return (
+                <div className={constructorStyle.constructor_position} key={element._id} onClick={() => props.onClick(element)}>
+                    <ConstructorElement
+                        key={element._id}
+                        type="top"
+                        isLocked={true}
+                        text={`${element.name} (верх)`}
+                        price={element.price}
+                        thumbnail={element.image}
+                    />
+                </div>
+            )
+        }
+    })
+
+    const allIngredients = props.arrData.map(element => {
+        if (element.name !== 'Краторная булка N-200i') {
+            return (
+                <div className={constructorStyle.constructor__flexContainer} key={element._id} onClick={() => props.onClick(element)}>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        key={element._id}
+                        text={element.name}
+                        price={element.price}
+                        thumbnail={element.image}
+                    />
+                </div>
+            )
+        }
+    })
+
+    const bottomBun = props.arrData.map(element => {
+        if (element.name === 'Краторная булка N-200i') {
+            return (
+                <div className={constructorStyle.constructor_position} key={element._id} onClick={() => props.onClick(element)}>
+                    <ConstructorElement
+                        key={element._id}
+                        type="bottom"
+                        isLocked={true}
+                        text={`${element.name} (низ)`}
+                        price={element.price}
+                        thumbnail={element.image}
+                    />
+                </div>
+            )
+        }
+    })
+
+
+    /*const constructorElements = props.arrData.map(element => {
         if (element.name === 'Краторная булка N-200i') {
             return (
                 <div className={constructorStyle.constructor_position} key={element._id} onClick={() => props.onClick(element)}>
@@ -46,12 +97,16 @@ function BurgerConstructor(props) {
         } else {
             return null;
         }
-    })
+    }) */
 
     return (
         <section className={`${constructorStyle.constructor} mt-25`}>
-            <div className={`${constructorStyle.constructor__boxList} ${constructorStyle.constructor__boxList_scrollbar}`}>
-                {constructorElements}
+            <div className={`${constructorStyle.constructor__boxList}`}>
+                {topBun}
+                <div className={`${constructorStyle.constructor__boxList} ${constructorStyle.constructor__boxList_scrollbar}`}>
+                    {allIngredients}
+                </div>
+                {bottomBun}
             </div>
             <div className={`${constructorStyle.constructor__info} mt-10`}>
                 <div>
