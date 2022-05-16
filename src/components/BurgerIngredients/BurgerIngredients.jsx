2fc720a3d567c32113d514ac0bd343ca8f8d5692
Artrudjob <Artrudjob@yandex.ru>
2022-05-16 11:38:01 +0300

    feat: add anchor links

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -47,26 +47,32 @@ function BurgerIngredients(props) {
         <section className={burgersStyle.burgersMenu} onScroll={scrollIngredients}>
             <h1 className={'text text_type_main-large mt-10'}>Соберите бургер</h1>
             <div className={`${burgersStyle.burgersMenu__flex} mt-5`}>
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
-                    Булки
-                </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
-                    Соусы
-                </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
-                    Начинки
-                </Tab>
+                <a href={"#bun"} className={burgersStyle.burgersMenu__link}>
+                    <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                        Булки
+                    </Tab>
+                </a>
+                <a href={"#sauce"} className={burgersStyle.burgersMenu__link}>
+                    <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                        Соусы
+                    </Tab>
+                </a>
+                <a href={"#main"} className={burgersStyle.burgersMenu__link}>
+                    <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                        Начинки
+                    </Tab>
+                </a>
             </div>
             <div className={burgersStyle.burgersMenu_scrollbar} ref={refItem}>
-                <h2 className={'text text_type_main-medium mt-10'}>Булки</h2>
+                <h2 className={'text text_type_main-medium mt-10'} id={"bun"}>Булки</h2>
                 <div className={`${burgersStyle.burgersMenu__gridContainer} mt-6`}>
                     {bunsList}
                 </div>
-                <h2 className={'text text_type_main-medium mt-15'}>Соусы</h2>
+                <h2 className={'text text_type_main-medium mt-15'} id={"sauce"}>Соусы</h2>
                 <div className={`${burgersStyle.burgersMenu__gridContainer} mt-6`}>
                     {saucesList}
                 </div>
-                <h2 className={'text text_type_main-medium mt-10'}>Начинки</h2>
+                <h2 className={'text text_type_main-medium mt-10'} id={"main"}>Начинки</h2>
                 <div className={`${burgersStyle.burgersMenu__gridContainer} mt-6`}>
                     {toppingsList}
                 </div>
