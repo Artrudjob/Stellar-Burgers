19de6472ada3ee57f79d5212b7633126319f69dd
Artrudjob <Artrudjob@yandex.ru>
2022-05-24 19:56:52 +0300

    feat: add active link change

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,31 +1,59 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import {NavLink, Outlet} from 'react-router-dom';
 import appHeaderStyle from './appHeader.module.css';
 import { BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function AppHeader() {
+
     return (
         <>
             <section className={appHeaderStyle.header}>
                 <header className={appHeaderStyle.header__container}>
                     <nav className={appHeaderStyle.header__containerLinks}>
-                        <Link className={`${appHeaderStyle.header__link} pl-4 pr-4 pb-4 pt-4`} to="/">
-                            <BurgerIcon type={"primary"} />
-                            <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Конструктор</p>
-                        </Link>
-                        <Link className={`${appHeaderStyle.header__link} pl-4`} to="orderFeed">
-                            <ListIcon type="secondary" />
-                            <p className={`text text_type_main-default text_color_inactive p-2 ${appHeaderStyle.header__text}`}>Лента заказов</p>
-                        </Link>
+                        <NavLink className={`${appHeaderStyle.header__link} pl-4 pr-4 pb-4 pt-4`} to="/">
+                            { ({isActive}) => isActive ?
+                                <>
+                                    <BurgerIcon type={"primary"} />
+                                    <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Конструктор</p>
+                                </>
+                                :
+                                <>
+                                    <BurgerIcon type={"secondary"} />
+                                    <p className={`text text_type_main-default text_color_inactive p-2 ${appHeaderStyle.header__text}`}>Конструктор</p>
+                                </>
+                            }
+                        </NavLink>
+                        <NavLink className={`${appHeaderStyle.header__link} pl-4`} to="orderFeed">
+                            { ({isActive}) => isActive ?
+                                <>
+                                    <ListIcon type="primary" />
+                                    <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Лента заказов</p>
+                                </>
+                                :
+                                <>
+                                    <ListIcon type="secondary" />
+                                    <p className={`text text_type_main-default text_color_inactive p-2 ${appHeaderStyle.header__text}`}>Лента заказов</p>
+                                </>
+                            }
+                        </NavLink>
                     </nav>
                     <div>
                         <Logo />
                     </div>
                     <nav className={appHeaderStyle.header__containerLinks}>
-                        <Link className={`${appHeaderStyle.header__link}`} to="/login">
-                            <ProfileIcon type={"secondary"} />
-                            <p className={`text text_type_main-default text_color_inactive p-2 ${appHeaderStyle.header__text}`}>Личный кабинет</p>
-                        </Link>
+                        <NavLink className={`${appHeaderStyle.header__link}`} to="profile">
+                            { ({isActive}) => isActive ?
+                                <>
+                                    <ProfileIcon type={"primary"} />
+                                    <p className={`text text_type_main-default p-2 ${appHeaderStyle.header__text}`}>Личный кабинет</p>
+                                </>
+                                :
+                                <>
+                                    <ProfileIcon type={"secondary"} />
+                                    <p className={`text text_type_main-default text_color_inactive p-2 ${appHeaderStyle.header__text}`}>Личный кабинет</p>
+                                </>
+                            }
+                        </NavLink>
                     </nav>
                 </header>
             </section>
