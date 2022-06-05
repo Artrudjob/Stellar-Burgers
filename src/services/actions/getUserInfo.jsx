5f9093d86ffabf70ffb458c3bd27ead414bcd608
Artrudjob <Artrudjob@yandex.ru>
2022-06-05 13:51:39 +0300

    refactor: pass the accessToken obtained from cookie

--- src/services/actions/getUserInfo.jsx
+++ src/services/actions/getUserInfo.jsx
@@ -1,5 +1,5 @@
 import {baseUrl, checkResponse} from "../../consts/consts";
-import {getValidToken} from './getToken';
+import { getCookie } from '../../consts/consts';
 
 const GET_USER_INFO = 'GET_USER_INFO';
 
@@ -14,7 +14,7 @@ const getUserInfo = (userEmail, userName) => ({
 const fetchGetUserInfo = () => {
     return function (dispatch) {
 
-        const accessToken = localStorage.getItem('accessToken');
+        const accessToken= getCookie('accessToken');
 
         fetch(`${baseUrl}auth/user`, {
             method: 'GET',
