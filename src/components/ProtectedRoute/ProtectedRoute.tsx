56a052069c69ef8a4f7bd55e6b28d4f4dbd80cc1
Artrudjob <Artrudjob@yandex.ru>
2022-07-08 17:47:30 +0300

    refactor: change component ProtectedRoute from jsx to tsx

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import {useSelector} from 'react-redux';
 import {Navigate, useLocation} from 'react-router-dom';
+import {RootState} from '../../services/rootReducer';
 
 //компонент доступа к маршруту. Если пользователь не авторизирован, то идет переадресация
 // на незащищенный маршрут. Если авторизирован, то пользователь может перейти на защищенный маршрут.
-function ProtectedRoute({children, ...rest}) {
+
+interface IProps {
+    children?: React.ReactNode;
+}
+
+const ProtectedRoute = ({children}: IProps) => {
     const location = useLocation();
 
-    const userData = useSelector(store => store.authReducer); //получаем из хранилища данные о пользователе(авторизован ли он, его email и его имя)
+    const userData = useSelector((store: RootState) => store.authReducer); //получаем из хранилища данные о пользователе(авторизован ли он, его email и его имя)
 
     return (
         userData.isAuthorization ?
