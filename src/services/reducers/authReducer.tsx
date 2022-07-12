09746d1a8daddf24c3235ec15048f808111291e0
Artrudjob <Artrudjob@yandex.ru>
2022-07-12 16:41:01 +0300

    refactor: change authReducer from jsx to tsx

--- src/services/reducers/authReducer.tsx
+++ src/services/reducers/authReducer.tsx
@@ -1,19 +1,23 @@
-import {POST_AUTH} from '../actions/postAuth';
-import {REGISTER_USER} from '../actions/resgisterUser';
-import {GET_USER_INFO} from '../actions/getUserInfo';
-import {UPDATE_USER_INFO} from '../actions/updateUserInfo';
-import {POST_USER_EMAIL} from '../actions/postUserEmail';
-import {POST_NEW_PASSWORD} from '../actions/postNewPassword';
-import {SIGN_OUT_ACCOUNT} from '../actions/signOutAccount';
+import {
+    POST_AUTH,
+    POST_AUTH_FAILURE,
+    REGISTER_USER,
+    RESISTER_USER_FAILURE,
+    GET_USER_INFO,
+    GET_USER_INFO_FAILURE,
+    UPDATE_USER_INFO,
+    UPDATE_USER_FAILURE,
+    POST_USER_EMAIL,
+    POST_USER_EMAIL_FAILURE,
+    POST_NEW_PASSWORD,
+    POST_NEW_PASSWORD_FAILURE,
+    SIGN_OUT_ACCOUNT,
+    SIGN_OUT_ACCOUNT_FAILURE,
+    TAuthActions
+} from '../actions/authActions';
+import {IUserData} from '../interface/interface';
 
-import {POST_AUTH_FAILURE} from '../actions/postAuth';
-import {RESISTER_USER_FAILURE} from '../actions/resgisterUser';
-import {GET_USER_INFO_FAILURE} from '../actions/getUserInfo';
-import {UPDATE_USER_FAILURE} from '../actions/updateUserInfo';
-import {POST_USER_EMAIL_FAILURE} from '../actions/postUserEmail';
-import {POST_NEW_PASSWORD_FAILURE} from '../actions/postNewPassword';
-
-const initialState = {
+const initialState: IUserData = {
     isAuthorization: false,
     name: '',
     email: '',
@@ -22,7 +26,7 @@ const initialState = {
     message: ''
 }
 
-function authReducer(state = initialState, action) {
+function authReducer(state = initialState, action: TAuthActions) {
     switch (action.type) {
         case POST_AUTH:
         case REGISTER_USER:
@@ -54,6 +58,7 @@ function authReducer(state = initialState, action) {
         case UPDATE_USER_FAILURE:
         case POST_USER_EMAIL_FAILURE:
         case POST_NEW_PASSWORD_FAILURE:
+        case SIGN_OUT_ACCOUNT_FAILURE:
             return {
                 ...initialState
             };
